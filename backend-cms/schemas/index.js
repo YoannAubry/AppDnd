4965f84ded5a096c9cf9d9096712ffb06014783b
informatique <yoannmatthieuaubry@gmail.com>
2026-02-18 13:41:20 +0100

    Suppression de Sanity et passage en monorepo Next.js

--- backend-cms/schemas/index.js
+++
@@ -1,9 +0,0 @@
-import statBlock from './objects/statBlock'
-import monster from './monster'
-import player from './player'
-import faction from './faction'
-import npc from './npc'
-import location from './location'
-import campaign from './campaign'
-
-export const schemaTypes = [statBlock, monster, player, faction, npc, location, campaign]
